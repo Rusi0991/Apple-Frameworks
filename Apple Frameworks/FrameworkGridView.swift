1f2b3ad4e2267815f27cc71b94018a2c59b6a3c5
Ruslan Ismayilov <ruslanismayilovv@icloud.com>
2023-06-17 09:07:52 -0700

    Project Setup

---
+++ Apple Frameworks/FrameworkGridView.swift
@@ -0,0 +1,20 @@
+//
+//  FrameworkGridView.swift
+//  Apple Frameworks
+//
+//  Created by Ruslan Ismayilov on 6/17/23.
+//
+
+import SwiftUI
+
+struct FrameworkGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FrameworkGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        FrameworkGridView()
+    }
+}
