6748a7bfdc78a99284fd0a41be4b0460a37dfd28
Ruslan Ismayilov <ruslanismayilovv@icloud.com>
2023-06-21 13:20:40 -0700

    FrameworkDetailView added

---
+++ Apple Frameworks/FrameworkDetailView.swift
@@ -0,0 +1,43 @@
+//
+//  FrameworkDetailView.swift
+//  Apple Frameworks
+//
+//  Created by Ruslan Ismayilov on 6/21/23.
+//
+
+import SwiftUI
+
+struct FrameworkDetailView: View {
+    var framework : Framework
+    
+    var body: some View {
+        VStack{
+            Spacer()
+            FrameworkTitleView(framework: framework)
+            Text(framework.description)
+                .font(.body)
+                .padding()
+            
+            Spacer()
+            Button{
+                
+            } label: {
+                Text("Learn More")
+                    .font(.title2)
+                    .fontWeight(.semibold)
+                    .frame(width: 280, height: 50)
+                    .background(.red)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+                    
+            }
+        }
+    }
+}
+
+struct FrameworkDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        FrameworkDetailView(framework: MockData.sampleFramework)
+            .preferredColorScheme(.dark)
+    }
+}
