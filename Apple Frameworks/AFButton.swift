d409d2b23477ee654d957f6df48a13f20d539afb
Ruslan Ismayilov <ruslanismayilovv@icloud.com>
2023-06-22 11:14:39 -0700

    Detail View code refactoring

---
+++ Apple Frameworks/AFButton.swift
@@ -0,0 +1,29 @@
+//
+//  AFButton.swift
+//  Apple Frameworks
+//
+//  Created by Ruslan Ismayilov on 6/22/23.
+//
+
+import SwiftUI
+
+struct AFButton: View {
+    var title : String
+    var body: some View {
+        
+    
+        Text(title)
+            .font(.title2)
+            .fontWeight(.semibold)
+            .frame(width: 280, height: 50)
+            .background(.red)
+            .foregroundColor(.white)
+            .cornerRadius(10)
+    }
+}
+
+struct AFButton_Previews: PreviewProvider {
+    static var previews: some View {
+        AFButton(title: "Test title")
+    }
+}
--- Apple Frameworks/FrameworkDetailView.swift
+++ Apple Frameworks/FrameworkDetailView.swift
@@ -12,32 +12,43 @@ struct FrameworkDetailView: View {
     
     var body: some View {
         VStack{
-            Spacer()
-            FrameworkTitleView(framework: framework)
-            Text(framework.description)
-                .font(.body)
-                .padding()
-            
-            Spacer()
-            Button{
-                
-            } label: {
-                Text("Learn More")
-                    .font(.title2)
-                    .fontWeight(.semibold)
-                    .frame(width: 280, height: 50)
-                    .background(.red)
-                    .foregroundColor(.white)
-                    .cornerRadius(10)
+            HStack{
+                Spacer()
+                Button{
                     
+                } label: {
+                    Image(systemName: "xmark")
+                        .foregroundColor(Color(.label))
+                        .imageScale(.large)
+                        .frame(width: 44, height: 44)
+                        .background(.gray)
+                        .cornerRadius(50)
+                        
+                }
+            }
+               .padding()
+            
+                Spacer()
+                FrameworkTitleView(framework: framework)
+                Text(framework.description)
+                    .font(.body)
+                    .padding()
+                
+                Spacer()
+                Button{
+                    
+                } label: {
+                    AFButton(title: "Learn more")
+                    
+                }
             }
         }
     }
-}
-
-struct FrameworkDetailView_Previews: PreviewProvider {
-    static var previews: some View {
-        FrameworkDetailView(framework: MockData.sampleFramework)
-            .preferredColorScheme(.dark)
+    
+    struct FrameworkDetailView_Previews: PreviewProvider {
+        static var previews: some View {
+            FrameworkDetailView(framework: MockData.sampleFramework)
+                .preferredColorScheme(.dark)
+        }
     }
-}
+
